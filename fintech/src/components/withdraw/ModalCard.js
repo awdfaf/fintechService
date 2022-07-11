7fe4055d30de0d35665731790687f9d07a2c3623
awdfaf <awdfaf@kakao.com>
2022-07-11 17:07:04 +0900

    출금.입금 이체 완료

--- fintech/src/components/withdraw/ModalCard.js
+++ fintech/src/components/withdraw/ModalCard.js
@@ -32,7 +32,7 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "T991599190U" + countnum; //이용기과번호 본인것 입력
+    let transId = "M202201320U" + countnum; //이용기과번호 본인것 입력
     return transId;
   };    
      
@@ -43,7 +43,7 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
     const sendData = {
         "bank_tran_id": genTransId(),
         "cntr_account_type": "N",
-        "cntr_account_num": "7832932596",
+        "cntr_account_num": "100000000001",
         "dps_print_content": "쇼핑몰환불",
         "fintech_use_num": fintechUseNo,
         "wd_print_content": "오픈뱅킹출금",
@@ -53,9 +53,9 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
         "req_client_fintech_use_num": fintechUseNo,
         "req_client_num": "HONGGILDONG1234",
         "transfer_purpose": "TR",
-        "recv_client_name": "진상언",
+        "recv_client_name": "김오픈",
         "recv_client_bank_code": "097",
-        "recv_client_account_num": "7832932596"    
+        "recv_client_account_num": "100000000001"    
     }
 
     const option = {
@@ -74,14 +74,14 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   } 
 
   const deposit = () => {
-    const twoLeggedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJUOTkxNTk5MTkwIiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNjYwODA0NTE4LCJqdGkiOiJiMTY1YmQzMi1jYzM1LTQyNzgtODFjZi01OTY2OTFlZjMzOGYifQ.YfQA8mf_S-PZvODqZ-i4okO5jXtOlb42PkaiufsihNw";
+    const twoLeggedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJNMjAyMjAxMzIwIiwic2NvcGUiOlsib29iIl0sImlzcyI6Imh0dHBzOi8vd3d3Lm9wZW5iYW5raW5nLm9yLmtyIiwiZXhwIjoxNjY1MzAwMjIyLCJqdGkiOiI1ZmZlZWI3MS02NTkwLTQ1Y2EtYWRiNy1jMjEzOTJhOWI1ZWYifQ.7dgaAFThjKwgzCoub9E2Vrj_Dndq6goVoLK9zMimQlc";
     const sendData = {
         cntr_account_type: "N",
-        cntr_account_num: "4262679045",
+        cntr_account_num: "200000000001",
         wd_pass_phrase: "NONE",
         wd_print_content: "환불금액",
         name_check_option: "off",
-        tran_dtime: "20190910101921",
+        tran_dtime: "20220710101921",
         req_cnt: "1",
         req_list: [
           {
